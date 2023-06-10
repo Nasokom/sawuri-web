e3984d28ef6279ef73da4c3b7f9b48322de1993d
Mike <124715146+Nasokom@users.noreply.github.com>
2023-06-10 23:19:01 +0200

    kl

--- sawuri-next/component/mailTemplate/EmailTemplate.jsx
+++ sawuri-next/component/mailTemplate/EmailTemplate.jsx
@@ -1,5 +1,7 @@
 import React,{useState,useEffect} from 'react'
 import ComplexText from '../Ui/ComplexText';
+import fs from 'fs';
+import path from 'path';
 
 function EmailTemplate({ name, message,ourResponse, userLang}) {
 
@@ -30,13 +32,24 @@ function EmailTemplate({ name, message,ourResponse, userLang}) {
   }
 
   const line ={
-    backgroundColor:'#f9d1a3',
+    backgroundColor:'#f7ede3',
     width:'100%',
     height:'10px',
     borderRadius:'30px',
     marginTop:'40px',
     marginBottom: '40px'
   }
+
+
+/*   const imagePath = '/public/marcel_logo.png';
+
+// Lire l'image en tant que buffer
+const imageBuffer = fs.readFileSync(imagePath);
+
+// Convertir le buffer en chaîne Base64
+const base64Image = imageBuffer.toString('base64');
+  const imageSrc = `data:image/png;base64,${base64Image}`;
+ */
     return (
       <html style={{...html,...base}}>
         <head>
@@ -45,14 +58,17 @@ function EmailTemplate({ name, message,ourResponse, userLang}) {
         <body style={{...wrapperStyles,...base}}>
 
             <h1>{ourResponse.subject[userLang]}</h1>
-            
-            <span style={line}></span>
+
+            <div style={line}></div>
 
             <ComplexText texts={ourResponse.message[userLang]}/>
 
-            <span style={line}></span>
+            <div style={line}></div>
 
-            <img  style={img} src="https://sawuri.vercel.app/marcel_logo.jpg" alt="Image" />
+            <img  style={img} 
+            src="https://sawuri.vercel.app/marcel_logo.jpg" 
+            //src={imageSrc}
+            alt="Image" />
         </body>
       </html>
     );
